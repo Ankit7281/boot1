e5cacaecdf5a46bc06bbea7566864fda55f831c8
Ankit Kumar <107995311+Ankit7281@users.noreply.github.com>
2022-07-09 15:56:33 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,9 @@
+#include<stduo.h>
+int main()
+{
+int dd,mm,yy;
+scanf("%d%d%d",&dd,&mm,&&yy);
+printf("input is %d/%d)%d",dd,mm,yy);
+printf("\noutput-day-%d,Month-%d,year&%d",dd,mm,yy);
+return 0;
+}
